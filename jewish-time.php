9a86e27254ebc467c7ae3889de7a38337ab0fc1b
David Shanske <david@shanske.com>
2022-07-03 19:14:23 +0000

    Initial Commit

---
+++ jewish-time.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name: Jewish Time
+ * Plugin URI: https://github.com/dshanske/tempus-fugit
+ * Description: Hebrew/Jewish Time Enhancements for WordPress
+ * Author: David Shanske
+ * Author URI: https://david.shanske.com
+ * Text Domain: jewish-time
+ * Version: 0.0.1
+ */
+
+register_activation_hook( __FILE__, array( 'Jewish_Time_Plugin', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Jewish_Time_Plugin', 'deactivate' ) );
+add_action( 'upgrader_process_complete', array( 'Jewish_Time_Plugin', 'upgrader_process_complete' ), 10, 2 );
+
+add_action( 'plugins_loaded', array( 'Jewish_Time_Plugin', 'plugins_loaded' ) );
+add_action( 'init', array( 'Jewish_Time_Plugin', 'init' ) );
+
+class Jewish_Time_Plugin {
+
+	public static function plugins_loaded() {
+	}
+
+	public static function init() {
+	}
+
+	public static function upgrader_process_complete( $upgrade_object, $options ) {
+		$current_plugin_path_name = plugin_basename( __FILE__ );
+		if ( ( 'update' === $options['action'] ) && ( 'plugin' === $options['type'] ) ) {
+			foreach ( $options['plugins'] as $each_plugin ) {
+				if ( $each_plugin === $current_plugin_path_name ) {
+					flush_rewrite_rules();
+				}
+			}
+		}
+	}
+
+	public static function activate() {
+		flush_rewrite_rules();
+	}
+
+	public static function deactivate() {
+		flush_rewrite_rules();
+	}
+}
+
+
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Jewish Time ===
+Contributors: dshanske
+Tags: time, archive, date, hebrew, jewish
+Stable tag: trunk
+Requires at least: 4.9.9
+Requires PHP: 5.6
+Tested up to: 6.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A Collection of Enhancements to for Hebrew/Jewish Date and Time Handling
+
+== Description == 
+
+There are other date and time themed plugins for WordPress that handle the Hebrew calendar. But they vary in function and focus.
+
+== Installation ==
+
+Install and activate.
+
+== Privacy and Data Storage Notice ==
+
+This plugin stores no private data.
+
+== Frequently Asked Questions ==
+
+
+== Changelog ==
+
+= Version 1.0 (xxxx-xx-xx) =
+* Initial Release
