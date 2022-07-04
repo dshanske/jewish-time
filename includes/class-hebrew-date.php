5df7f69c21e49e1539a898b220bb6a3244906c99
David Shanske <david@shanske.com>
2022-07-04 01:31:54 +0000

    Include time and timezone in the Hebrew Date class

--- includes/class-hebrew-date.php
+++ includes/class-hebrew-date.php
@@ -5,10 +5,6 @@
  */
 class JT_Hebrew_Date {
 
-	/* Date Time Object
-	 */
-	protected $datetime;
-
 	/* Hebrew Year.
 	 */
 	protected $year;
@@ -29,38 +25,101 @@ class JT_Hebrew_Date {
 	 */
 	protected $day;
 
+	/* Day of Week.
+	 */
+	protected $day_of_week;
+
 
 	/* Days in Month.
 	 */
 	protected $days_in_month;
 
+	/* Hours.
+	 */
+	protected $hour = 0;
+
+	/* Minutes.
+	 */
+	protected $minute = 0;
+
+	/* Seconds.
+	 */
+	protected $second = 0;
+
+	/* Milliseconds.
+	 */
+	protected $millisecond = 0;
+
+	/* Timezone.
+	 *
+	 */
+	protected $timezone;
+
 	/*
 	 * Constructor.
 	 *
-	 * @param DateTime $datetime Date Time Object to convert to Hebrew Time.
+	 * @param DateTime|DateTimeImmutable $datetime Date Time Object to convert to Hebrew Time.
 	 */
 
-	public function __construct( $datetime ) {
-		if ( $datetime instanceof DateTime ) {
-			$this->datetime = DateTimeImmutable::createFromMutable( $datetime );
-		} else {
-			$this->datetime = $datetime;
+	public function __construct( $datetime = null ) {
+		if ( $datetime instanceof DateTimeInterface ) {
+			$this->create_from_datetime( $datetime );
 		}
+		if ( ! $this->timezone ) {
+			$this->timezone = wp_timezone();
+		}
+	}
 
-		$timestamp   = $datetime->getTimestamp();
-		$jd          = unixtojd( $timestamp );
-		$cal         = cal_from_jd( $jd, CAL_JEWISH );
-		$this->month = $cal['month'];
-		$month_names = cal_info( CAL_JEWISH )['months'];
+	public function set_hebrew_date( $year, $month, $day ) {
+		$this->year        = (int) $year;
+		$this->month       = (int) $month;
+		$this->day         = (int) $day;
+		$jd                = cal_to_jd( CAL_JEWISH, $year, $month, $day );
+		$this->day_of_week = (int) jddayofweek( $jd ) + 1;
+		$month_names       = cal_info( CAL_JEWISH )['months'];
 		if ( $this->is_leap_year() ) {
 			$month_names[7] = 'Adar';
 		}
 		$month_names             = apply_filters( 'jewish_time_month_names', $month_names );
 		$this->month_name        = $month_names[ $this->month ];
 		$this->hebrew_month_name = $this->hebrew_month_name( $this->month );
-		$this->day               = $cal['day'];
-		$this->year              = $cal['year'];
 		$this->days_in_month     = $this->cal_days_in_month();
+		return true;
+	}
+
+	public function set_time( $hour, $minute, $second = 0, $millisecond = 0 ) {
+		$this->hour        = $hour;
+		$this->minute      = $minute;
+		$this->second      = $second;
+		$this->millisecond = $millisecond;
+	}
+
+	public function to_datetime() {
+		$jd        = jewishtojd( $this->year, $this->month, $this->day );
+		$timestamp = jdtounix( $jd );
+		$datetime  = new DateTime();
+		$datetime->setTimeZone( $this->timezone );
+		$datetime->setTimestamp( $timestamp );
+		$datetime->setTime( $this->hour, $this->minute, $this->second, $this->millisecond );
+		return DateTimeImmutable::createFromMutable( $datetime );
+	}
+
+	protected function create_from_datetime( $datetime ) {
+		if ( ! $datetime instanceof DateTimeInterface ) {
+			return false;
+		}
+
+		$timestamp = $datetime->getTimestamp();
+		$jd        = unixtojd( $timestamp );
+		$cal       = cal_from_jd( $jd, CAL_JEWISH );
+		$this->set_hebrew_date( $cal['year'], $cal['month'], $cal['day'] );
+		$this->hour        = (int) $datetime->format( 'G' );
+		$this->minute      = (int) $datetime->format( 'i' );
+		$this->second      = (int) $datetime->format( 's' );
+		$this->day_of_week = (int) $datetime->format( 'w' ) + 1;
+		$this->millisecond = (int) $datetime->format( 'u' );
+		$this->timezone    = $datetime->getTimezone();
+		return true;
 	}
 
 	public function toArray() {
@@ -68,19 +127,19 @@ class JT_Hebrew_Date {
 	}
 
 	/*
-	 * Returns whether the year is a leap year
-	 *
-	 * @return boolean
-	 */
+	* Returns whether the year is a leap year
+	*
+	* @return boolean
+	*/
 	public function is_leap_year() {
 		return ( 1 + ( $this->year * 7 ) ) % 19 < 7 ? true : false;
 	}
 
 	/*
-	 * Wrapper around calendar day in months that returns the number of days in the provided month.
-	 *
-	 * @return int Days in Month.
-	 */
+	* Wrapper around calendar day in months that returns the number of days in the provided month.
+	*
+	* @return int Days in Month.
+	*/
 	public function cal_days_in_month() {
 		return cal_days_in_month( CAL_JEWISH, $this->month, $this->year );
 	}
@@ -108,7 +167,5 @@ class JT_Hebrew_Date {
 		);
 		return $months[ $this->month - 1 ];
 	}
-
-
 }
 
